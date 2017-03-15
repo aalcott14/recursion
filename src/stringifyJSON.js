c2446eadd7901a41aeb65a61bf9111814830fadc
Adam Alcott <aalcott14@gmail.com>
2017-03-15 00:04:24 -0500

    Stringifies numbers, booleans, and strings

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,22 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number' || obj === true || obj === false) {
+  	return obj.toString();
+  } else if (obj === null) {
+  	return String(obj);
+  } else if (typeof obj === 'string') {
+  	return '\"'+ obj + '\"';
+  } else if (typeof obj === 'object') {
+  	var objArray = Array.prototype.slice.call(obj);
+  	var result = [];
+  	if (obj.length === 0) {
+  		return '[]';
+  	} else if (objArray.length === 0) {
+  	  return result.toString();
+    } else {
+  	  result.push(objArray.pop());
+  	  stringifyJSON(objArray);
+    }
+  }	
 };
